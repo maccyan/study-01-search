bc9d6e0005faf0c3399381b1bb1ed67d212db858
maccyan <macya21@gmail.com>
2021-09-07 22:32:46 +0900

    ３．２に追加
    ３．２に追加してキャラクターリスト(source)をCSVから読み込んで登録

--- search.py
+++ search.py
@@ -2,9 +2,6 @@
 ### 検索ツールサンプル
 ### これをベースに課題の内容を追記してください
 
-# 検索ソース
-source=["ねずこ","たんじろう","きょうじゅろう","ぎゆう","げんや","かなお","ぜんいつ"]
-
 ### 検索ツール
 def search():
     word =input("鬼滅の登場人物の名前を入力してください >>> ")
@@ -14,8 +11,13 @@ def search():
         print("{}が見つかりした".format(word))
     else:
         print("存在しません")
-        source.append(word)
+#       source.append(word)
 
 
 if __name__ == "__main__":
-    search()
+
+    import csv
+    with open('source.csv','r',encoding='UTF-8') as f :
+        reader = csv.reader(f)
+        for source in reader:
+            search()
